98675449e6efe8863bca74543d381cc49abd25a1
takaya787 <takaya318318@gmail.com>
2020-11-02 15:47:18 +0900

    has_secure 調整中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #ファイルが変更をコンテナを再起動しなくても反映できるようにする
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end

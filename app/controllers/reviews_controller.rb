30ae99624f6d9baf60e3ab021ebecab1dba07213
takaya787 <takaya318318@gmail.com>
2020-11-15 15:21:26 +0900

    check.js編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,16 +9,21 @@ class ReviewsController < ApplicationController
 
   def check
     result = Geocoder.search(params[:keyword])
-    if !result.empty?
-      puts result
-      @result = result.first.coordinates
-      session[:lat] = @result[0]
-      session[:lng] = @result[1]
-      flash[:success]="検索した場所に移動します"
-      redirect_to "/reviews/new"
-    else
-      render 'reviews/new'
-      flash[:danger]="検索した場所は見つかりませんでした"
+    respond_to do |format|
+      if !result.empty?
+        puts result
+        @result = result.first.coordinates
+        @center = { lat: @result[0], lng: @result[1] }
+        session[:lat] = @result[0]
+        session[:lng] = @result[1]
+        flash[:success]="検索した場所に移動します"
+        format.html { redirect_to new_review_url}
+        format.js
+      else
+        format.html { render :new }
+        format.json { render "reviews/new" }
+        flash[:danger]="検索した場所は見つかりませんでした"
+      end
     end
   end
 end

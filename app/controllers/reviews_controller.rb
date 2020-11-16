fa68881240d08fcc290dd7812c14036d29859e54
takaya787 <takaya318318@gmail.com>
2020-11-16 22:22:45 +0900

    geocoderをsessionで管理しjsonで出力

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,18 @@
 class ReviewsController < ApplicationController
   def index
+
   end
 
   def new
-    @lat = session[:lat]
-    @lng = session[:lng]
+    if session[:lat] && session[:lng]
+      @center = { "lat" => session[:lat], "lng" => session[:lng]}
+    else
+      @center = { "lat" => 34.6937249, "lng" => 135.5022535 }
+    end
+    respond_to do |format|
+      format.html
+      format.json { render :json => @center}
+    end
   end
 
   def check
@@ -12,16 +20,18 @@ class ReviewsController < ApplicationController
     respond_to do |format|
       if !result.empty?
         puts result
-        result = result.first.coordinates
-        @center = { "lat" => result[0], "lng" => result[1] }
+        geography= result.first.coordinates
+        session[:lat] = geography[0]
+        session[:lng] = geography[1]
         #@center["lat"], @center["lng"]
         flash[:success]="検索した場所に移動します"
         format.html { redirect_to new_review_url}
-        format.js
+        format.json { geography }
+        #format.js {flash[:success]= "検索した場所は見つかりませんでした"}
       else
         format.html { render :new }
-        format.js { render "reviews/new" }
-        flash[:danger]="検索した場所は見つかりませんでした"
+        format.json { }
+        #format.js { flash[:danger]= "検索した場所は見つかりませんでした"}
       end
     end
   end

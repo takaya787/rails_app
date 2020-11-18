a5641c5826d5f89c691fe6785680ee6d3b42d2f9
takaya787 <takaya318318@gmail.com>
2020-11-18 11:07:09 +0900

    spotのcolumn修正して,geocoderを設置

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,6 @@
 class Spot < ApplicationRecord
-  belongs_to　:review
+  belongs_to :review
+  #lat, lonにcolumnを変更
+  geocoded_by :adress, latitude: :lat, longitude: :lon
+  after_validation :geocode
 end

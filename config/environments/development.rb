5a197b9cf1b67278b034291348033ba98dd16fe5
takaya787 <takaya318318@gmail.com>
2020-11-20 00:28:20 +0900

    geocoderとdevelopmentのconfig file 修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #Cannot render consoleを防ぐためにDocker containerのgatewayを登録する(qiita参照)
+  config.web_console.whitelisted_ips = '172.19.0.1'
+
   # Do not eager load code on boot.
   config.eager_load = false
 

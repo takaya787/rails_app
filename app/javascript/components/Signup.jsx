572d804e49b1908434630260a7538f23880a1c81
takaya787 <takaya318318@gmail.com>
2020-11-21 12:02:30 +0900

    signup用formのデザイン構築完了して、全体のレイアウトも変更した

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -8,13 +8,16 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isModalOpen: false,
+      isSignupOpen: false,
+      isLoginOpen: false,
       user_name: '',
       user_email: '',
       user_password: '',
       user_password_confirmation: '',
     };
-    this.modalcontroll = this.modalcontroll.bind(this);
+    this.signupcontroll = this.signupcontroll.bind(this);
+    this.logincontroll = this.logincontroll.bind(this);
+    this.closemodal = this.closemodal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -29,23 +32,36 @@ class Signup extends React.Component {
     console.log(this.event.state)
     event.preventDefault();
   }
-
-  modalcontroll(event) {
+  closemodal(event) {
     this.setState({
-      isModalOpen: !this.state.isModalOpen,
+      isSignupOpen: false,
+      isLoginOpen: false,
+    });
+  }
+  signupcontroll(event) {
+    this.setState({
+      isSignupOpen: !this.state.isSignupOpen,
+      isLoginOpen: false,
+    });
+  }
+  logincontroll(event) {
+    this.setState({
+      isLoginOpen: !this.state.isLoginOpen,
+      isSignupOpen: false,
     });
   }
   render() {
-    if (this.state.isModalOpen) {
+    if (this.state.isSignupOpen && !this.state.isLoginOpen) {
       return (
         <div className="modal">
           <form
+            id="signup-modal"
             className="form"
             onSubmit={this.handleSubmit}
-            action={this.props.url} acceptCharset="UTF-8" method="post"
+            action={this.props.signup} acceptCharset="UTF-8" method="post"
             remote="true">
-            <button onClick={this.modalcontroll}>✕</button>
-            <h2 className="form_title">まずはSign up!</h2>
+            <button className="close" onClick={this.closemodal}>✕</button>
+            <h2 className="form_title">まずはユーザー登録!</h2>
             <input type="hidden" name="authenticity_token"
               value={this.props.authenticityToken} />
 
@@ -59,8 +75,10 @@ class Signup extends React.Component {
             <label htmlFor="user_password_confirmation">パスワード確認用</label>
             <input className="form_input" type="password" name="user[password_confirmation]" id="user_password_confirmation" value={this.state.user_password_confirmation} onChange={this.handleChange} />
 
-            <input className="form_submit" type="submit" name="submit" value="提出する" />
-
+            <input className="form_submit" type="submit" name="submit" value="登録する" />
+            <button className="login" onClick={this.logincontroll}>
+              Loginはこちら
+            </button>
             <a className="form_google" href="/auth/google_oauth2">
               <FcGoogle size={20} />
               <span className="description" >GoogleアカウントでLog in</span>
@@ -68,16 +86,51 @@ class Signup extends React.Component {
           </form>
         </div>
       );
-    } else {
+    }
+    else if (this.state.isLoginOpen && !this.state.isSignupOpen) {
       return (
-        <button className="initial-button" onClick={this.modalcontroll}>はじめる</button>
+        <div className="modal">
+          <form
+            id="login-modal"
+            className="form"
+            onSubmit={this.handleSubmit}
+            action={this.props.login} acceptCharset="UTF-8" method="post"
+            remote="true">
+            <button className="close" onClick={this.closemodal}>✕</button>
+            <h2 className="form_title">Log inはこちら！</h2>
+            <input type="hidden" name="authenticity_token"
+              value={this.props.authenticityToken} />
+
+            <label htmlFor="user_email">Eメール</label>
+            <input className="form_input" type="text" name="user[email]" id="user_email" value={this.state.user_email} onChange={this.handleChange} />
+            <label htmlFor="user_password">パスワード</label>
+            <input className="form_input" type="password" name="user[password]" id="user_password" value={this.state.user_password} onChange={this.handleChange} />
+            <label htmlFor="user_password_confirmation">パスワード確認用</label>
+            <input className="form_input" type="password" name="user[password_confirmation]" id="user_password_confirmation" value={this.state.user_password_confirmation} onChange={this.handleChange} />
+
+            <input className="form_submit" type="submit" name="submit" value="Loginする" />
+            <button className="login" onClick={this.signupcontroll}>
+              Signupはこちら
+            </button>
+            <a className="form_google" href="/auth/google_oauth2">
+              <FcGoogle size={20} />
+              <span className="description" >GoogleアカウントでLogin</span>
+            </a>
+          </form>
+        </div>
+      );
+    }
+    else {
+      return (
+        <button className="initial" onClick={this.signupcontroll}>はじめる</button>
       );
     }
   }
 }
 
 Signup.propTypes = {
-  url: PropTypes.string,
+  signup: PropTypes.string,
+  login: PropTypes.string,
   authenticityToken: PropTypes.string
 };
 export default Signup

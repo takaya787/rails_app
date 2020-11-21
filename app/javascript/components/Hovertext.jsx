cd5a94a3078c26e00bdad8b12be6d42009d0356e
takaya787 <takaya318318@gmail.com>
2020-11-21 16:20:02 +0900

    hoverpinの表示テキスト　設置

---
+++ app/javascript/components/Hovertext.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Hovertext extends React.Component {
+  render() {
+    return (
+      <div id="pop">
+        <div className="text">
+          <p>Clickして投稿しよう！</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hovertext

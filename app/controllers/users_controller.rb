fac07722921b7d74937927a6bcd42cd37985e558
takaya787 <takaya318318@gmail.com>
2020-11-24 21:49:48 +0900

    userとsession newのエラーメッセージのデザイン変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,7 @@ class UsersController < ApplicationController
         current_user
       else
         #format.html { render 'controller/name' }でもいける
+        flash.now[:danger] = "ユーザーを作成できませんでした。"
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end

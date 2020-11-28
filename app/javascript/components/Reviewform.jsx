196afd66830dc931fe5b03cb5580f0ef0e40e7f2
takaya787 <takaya318318@gmail.com>
2020-11-28 17:20:42 +0900

    reviewformからpostしてcontrollerでcreateまで完了

--- app/javascript/components/Reviewform.jsx
+++ app/javascript/components/Reviewform.jsx
@@ -7,9 +7,11 @@ class Reviewform extends React.Component {
     this.state = {
       review_reason: '',
       review_duration: 1,
-      review_good: '',
-      review_bad: '',
+      review_food: '',
+      review_convenient: '',
+      review_favorite: '',
       review_advice: '',
+      review_score: 0,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -33,9 +35,11 @@ class Reviewform extends React.Component {
         review: {
           reason: this.state.review_reason,
           duration: this.state.review_duration,
-          good: this.state.review_good,
-          bad: this.state.review_bad,
+          food: this.state.review_food,
+          convenient: this.state.review_convenient,
+          favorite: this.state.review_favorite,
           advice: this.state.review_advice,
+          score: this.state.review_score
         },
         lat: this.props.lat,
         lng: this.props.lng,
@@ -75,13 +79,21 @@ class Reviewform extends React.Component {
           <label htmlFor="review_duration">期間(月)　<span className="required">＊必須</span></label>
           <input className="form" type="number" name="review[duration]" id="review_duration" value={this.state.review_duration} onChange={this.handleChange} required />
 
-          <label htmlFor="review_good">住んで良かったこと(150字以内)</label>
-          <textarea className="form textarea" name="review[good]" id="review_good" value={this.state.review_good} onChange={this.handleChange} />
+          <label htmlFor="review_food">食生活について教えてください(150字以内)</label>
+          <textarea className="form textarea" name="review[food]" id="review_food" value={this.state.review_food} onChange={this.handleChange} />
 
-          <label htmlFor="review_bad">住んで嫌だったこと(150字以内)</label>
-          <textarea className="form textarea" name="review[bad]" id="review_bad" value={this.state.review_bad} onChange={this.handleChange} />
+          <label htmlFor="review_convenient">インフラなどの利便性について教えてください(150字以内)</label>
+          <textarea className="form textarea" name="review[convenient]" id="review_convenient" value={this.state.review_convenient} onChange={this.handleChange} />
+
+          <label htmlFor="review_favorite">あなたのお気に入りについて教えてください(150字以内）</label>
+          <input className="form" type="text" name="review[favorite]" id="review_favorite" placeholder="現地のお気に入りのお店、場所、食べ物について教えてください" value={this.state.review_favorite} onChange={this.handleChange}
+          />
+
           <label htmlFor="review_advice">次に来る人へのアドバイス(150字以内)</label>
           <textarea className="form textarea" name="review[advice]" id="review_advice" value={this.state.review_advice} onChange={this.handleChange} />
+
+          <label htmlFor="review_score">住み心地はいかがでしたか？　<span className="required">＊必須</span></label>
+          <input className="form" type="number" name="review[score]" id="review_score" value={this.state.review_score} onChange={this.handleChange} required />
 
           <button className="form_submit" type="button" name="submit" onClick={this.handleSubmit}>投稿を送信</button>
         </form>

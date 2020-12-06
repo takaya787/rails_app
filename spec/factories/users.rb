abd382365d33c89528db8d64958c8f97620b586a
takaya787 <takaya318318@gmail.com>
2020-12-06 15:35:24 +0900

    userをfind_or_createでbotに設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
+    reviews {[
+      FactoryBot.build(:review, user: nil)
+    ]}
     name { "user" }
     email { "testuser@example.com" }
     password { "foobar" }
     password_confirmation { "foobar" }
+    initialize_with { User.find_or_create_by(email: email)}
   end
   factory :host, class: User do
     name { "host" }
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   describe "reviewのvalidatesを検証" do
-    # before do
-    #   FactoryBot.create(:user)
-    #   FactoryBot.create(:host)
-    #   user = User.find_by(email: "testuser@example.com")
-    #   session[:user_id]= user.id
-    #   @current_user = user
-    #   @host = User.find_by(email: "testhost@example.com")
-    # end
-    it "loginなしでreviewは作成できない" do
+    #userとしてlogin
+    before do
+      FactoryBot.create(:user)
+      FactoryBot.create(:host)
+      user = User.find_by(email: "testuser@example.com")
+      @current_user = user
+      @host = User.find_by(email: "testhost@example.com")
+    end
+    it "reviewが作成できる" do
       before_counts = Review.count
-      review = Review.create(
-        reason: "test review",
-        duration: 1,
-        advice: "this is a test advice",
-        food: "this is a test food column",
-        convenient: "this is a test convenient column ",
-        favorite: "this is a test favorite column",
-        score: 2.5,
-      )
+      # review = @current_user.reviews.create(
+      #   reason: "test review",
+      #   duration: 1,
+      #   advice: "this is a test advice",
+      #   food: "this is a test food column",
+      #   convenient: "this is a test convenient column ",
+      #   favorite: "this is a test favorite column",
+      #   score: 2.5,
+      # )
+      review = FactoryBot.create(:review)
       after_counts = Review.count
       review.destroy
-      expect(after_counts - before_counts).to eq 0
+      expect(after_counts - before_counts).to eq 1
+      expect(review.user_id).to eq @current_user.id
     end
   end
 end
